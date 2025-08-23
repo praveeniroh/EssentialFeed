a5ddc652d5ba445eadd582091db03a027ef2bd15
iroh <pravsweee@gmail.com>
2025-08-23 23:44:00 +0530

    Common util getter added for tests

--- EssentialFeed/EssentialFeedTests/FeedsLoaderTest.swift
+++ EssentialFeed/EssentialFeedTests/FeedsLoaderTest.swift
@@ -10,19 +10,24 @@ import EssentialFeed
 
 final class RemoteFeedsLoaderTest: XCTestCase {
     func test_init_doNotRequestsDataFromURL() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(httpClient: client, url: URL(string:"https://example.com")!)
+        let (_,client) = getFeedLoaderAndClient()
         XCTAssertNil(client.url)
     }
 
     func test_load_requestsDataFromURL() {
-        let client = HTTPClientSpy()
         let url = URL( string:"https://example.com/feed")!
-        let loader = RemoteFeedLoader(httpClient: client, url: url)
+        let (loader, client) = getFeedLoaderAndClient(url: url)
         loader.load()
         XCTAssertNotNil(client.url)
     }
 
+    //MARK: Utils
+    private func getFeedLoaderAndClient(url: URL = URL(string: "https://example.com/feed")!) -> (loader: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let loader = RemoteFeedLoader(httpClient: client, url: url)
+        return (loader, client)
+
+    }
 
     private class HTTPClientSpy: HTTPClient {
         var url: URL?
