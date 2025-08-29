51606faf4cfb61fb4fcffe6373a7f09b06d0b5b0
iroh <pravsweee@gmail.com>
2025-08-29 06:59:42 +0530

    Moving mapping logic to new type FeedItemMapper

--- EssentialFeed/EssentialFeed/FeedLoader/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedLoader/RemoteFeedLoader.swift
@@ -39,9 +39,10 @@ public final class RemoteFeedLoader {
         httpClient.load(from: url, onCompletion: { response in
             switch response {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    onCompletion(.success(root.items.map{$0.feedItem}))
-                } else {
+                do {
+                    let feedItem = try FeedItemMapper.map(data, response)
+                    onCompletion(.success(feedItem))
+                } catch {
                     onCompletion(.failure(.invalidData))
                 }
             case .failure:
@@ -51,17 +52,28 @@ public final class RemoteFeedLoader {
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
+struct FeedItemMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
 
-private struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
 
-    var feedItem: FeedItem {
-        FeedItem(id: id, description: description, location: location, imageURL: image)
+        var feedItem: FeedItem {
+            FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+
+    static func map(_ data: Data, _ response: HTTPURLResponse)throws -> [FeedItem] {
+        if response.statusCode == 200 {
+            let root = try JSONDecoder().decode(Root.self, from: data)
+            return root.items.map(\.self.feedItem)
+        } else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
     }
 }
