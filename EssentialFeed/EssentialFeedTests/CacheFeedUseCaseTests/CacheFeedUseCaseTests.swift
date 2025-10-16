cd671eacaf99158e622f618a95b59946e89846c2
iroh <pravsweee@gmail.com>
2025-10-16 09:11:27 +0530

    Save commad requests cache deletion
    (cherry picked from commit b895bf00fb55a26a9112f18dac876e039c4ddcb7)

--- EssentialFeed/EssentialFeedTests/CacheFeedUseCaseTests/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/CacheFeedUseCaseTests/CacheFeedUseCaseTests.swift
@@ -6,12 +6,23 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 class LocalFeedLoader {
+    private let store: FeedStore
     init(store: FeedStore) {
+        self.store = store
+    }
+
+    func save(_ feedItems: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
@@ -21,5 +32,23 @@ final class CacheFeedUseCaseTests: XCTestCase {
         let _ = LocalFeedLoader(store: store)
 
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let feedItems = [uniqueItems(), uniqueItems()]
+        sut.save(feedItems)
+
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+
+    //MARK: - Helpers
+    private func uniqueItems() -> FeedItem {
+        FeedItem(id: UUID(), description: "Any", location: "Any", imageURL: anyURL())
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "https://some-url.com")!
     }
 }
