507bc17e8337ec9c5461779f3eabd7cb3fa84603
iroh <pravsweee@gmail.com>
2025-10-16 09:14:23 +0530

    Extracting System under test (SUT) to a factory method to protect against breaking changes
    (cherry picked from commit 649c44b46a4724a7a7b82e5d3d9bd3ca30207bae)

--- EssentialFeed/EssentialFeedTests/CacheFeedUseCaseTests/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/CacheFeedUseCaseTests/CacheFeedUseCaseTests.swift
@@ -28,15 +28,13 @@ class LocalFeedLoader {
 
 final class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesNotDeleteCacheUponDeletion() {
-        let store = FeedStore()
-        let _ = LocalFeedLoader(store: store)
+        let (_,store) = makeSUT()
 
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
 
     func test_save_requestsCacheDeletion() {
-        let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
+        let (sut, store) = makeSUT()
         let feedItems = [uniqueItems(), uniqueItems()]
         sut.save(feedItems)
 
@@ -44,6 +42,11 @@ final class CacheFeedUseCaseTests: XCTestCase {
     }
 
     //MARK: - Helpers
+    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        return (sut, store)
+    }
     private func uniqueItems() -> FeedItem {
         FeedItem(id: UUID(), description: "Any", location: "Any", imageURL: anyURL())
     }
