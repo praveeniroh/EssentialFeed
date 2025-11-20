42cf401d55380f25c233fc30b4f241e02b05cf5b
praveenraj t <praveenraj.ta@zohocorp.com>
2025-11-21 05:10:30 +0530

    Replacing production store url with test specific store URL to avoid sharing state/artifacts from other part of system (including other systems)

--- EssentialFeed/EssentialFeedTests/CacheFeedUseCaseTests/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/CacheFeedUseCaseTests/CodableFeedStoreTests.swift
@@ -62,11 +62,11 @@ class CodableFeedStoreTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        try? FileManager.default.removeItem(at: storeURL())
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 
     override func tearDown() {
-        try? FileManager.default.removeItem(at: storeURL())
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
         super.tearDown()
     }
 
@@ -126,12 +126,12 @@ class CodableFeedStoreTests: XCTestCase {
 
     //MARK: - Helpers
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
-        let sut = CodableFeedStore(storeURL: storeURL())
+        let sut = CodableFeedStore(storeURL: testSpecificStoreURL())
         trackForMemoryLead(sut, file: file, line: line)
         return sut
     }
 
-    private func storeURL() -> URL {
-        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    private func testSpecificStoreURL() -> URL {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
     }
 }
