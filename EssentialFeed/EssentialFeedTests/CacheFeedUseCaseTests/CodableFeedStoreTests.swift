9885e10c0c19bcd5d1c831ece963b834d3e6dd46
praveenraj t <praveenraj.ta@zohocorp.com>
2025-11-23 18:16:20 +0530

    Proved that `CodableFeedStore` side-effects run serially

--- EssentialFeed/EssentialFeedTests/CacheFeedUseCaseTests/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/CacheFeedUseCaseTests/CodableFeedStoreTests.swift
@@ -117,6 +117,30 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrive: .empty)
     }
 
+    func test_storeSideEffects_runSerailly(){
+        let sut = makeSUT()
+        var completedExpectationOrder = [XCTestExpectation]()
+        let op1 = expectation(description: "Operation 1")
+        sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
+            completedExpectationOrder.append(op1)
+            op1.fulfill()
+        }
+
+        let op2 = expectation(description: "Operation 2")
+        sut.deleteCachedFeed { _ in
+            completedExpectationOrder.append(op2)
+            op2.fulfill()
+        }
+
+        let op3 = expectation(description: "Operation 3")
+        sut.insert([], timestamp: Date()) { _ in
+            completedExpectationOrder.append(op3)
+            op3.fulfill()
+        }
+        waitForExpectations(timeout: 5.0)
+        XCTAssertEqual(completedExpectationOrder, [op1,op2,op3], "Expected to run side-effects serially but got a different order.")
+    }
+
     //MARK: - Helpers
     private func makeSUT(storeURL: URL? = nil,file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
         let sut = CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL())
