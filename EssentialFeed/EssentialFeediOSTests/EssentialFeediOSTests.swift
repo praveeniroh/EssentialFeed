fa8bed4bddc0895340d383d0b072b0b50163e70c
praveenraj t <praveenraj.ta@zohocorp.com>
2026-03-16 21:53:10 +0530

    Loading data on viewDidLoad only once

--- EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
+++ EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -8,12 +8,20 @@
 import XCTest
 
 class FeedViewController: UIViewController {
+    private let loader: EssentialFeediOSTests.LoaderSpy
     init(loader: EssentialFeediOSTests.LoaderSpy) {
+        self.loader = loader
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loader.load()
     }
 }
 
@@ -25,9 +33,18 @@ final class EssentialFeediOSTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 0)
     }
 
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+
     class LoaderSpy {
         private(set) var loadCallCount: Int = 0
 
-
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
