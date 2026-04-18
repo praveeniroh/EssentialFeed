80e07f4612d080329ba73c26f0b0e2459f8f67a1
praveenraj t <praveenraj.ta@zohocorp.com>
2026-04-18 12:49:55 +0530

    Making FeedImageCell Properties public

--- EssentialFeed/EssentialFeediOSTests/FeedImageCell.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedImageCell.swift
@@ -8,11 +8,11 @@
 import Foundation
 import UIKit
 
-class FeedImageCell: UITableViewCell {
-    let locationContainer = UIView()
-    let locationLabel = UILabel()
-    let descriptionLabel = UILabel()
-    let feedImageContainer = UIView()
-    let feedImageView = UIImageView()
-    let feedImageRetryButton = UIButton()
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+    public let feedImageContainer = UIView()
+    public let feedImageView = UIImageView()
+    public let feedImageRetryButton = UIButton()
 }
