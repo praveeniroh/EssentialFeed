cf1662459fd4cdf3f63afbd08b66966ddac7a5ad
praveenraj t <praveenraj.ta@zohocorp.com>
2026-05-14 07:08:35 +0530

    removing unused viewmodel

--- EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -6,28 +6,28 @@
 //
 
 import EssentialFeed
-
-
-final class FeedViewModel {
-    typealias Observer<T> = (T) -> Void
-    private let feedLoader: FeedLoader
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-
-    var onLoadingStateChange: Observer<Bool>?
-    var onFeedLoad: Observer<[FeedImage]>?
-
-    func loadFeed() {
-        onLoadingStateChange?(true)
-        feedLoader.load{[weak self] result in
-            guard let self else {
-                return
-            }
-            if let feed = try? result.get() {
-                onFeedLoad?(feed)
-            }
-            onLoadingStateChange?(false)
-        }
-    }
-}
+//
+//
+//final class FeedViewModel {
+//    typealias Observer<T> = (T) -> Void
+//    private let feedLoader: FeedLoader
+//    init(feedLoader: FeedLoader) {
+//        self.feedLoader = feedLoader
+//    }
+//
+//    var onLoadingStateChange: Observer<Bool>?
+//    var onFeedLoad: Observer<[FeedImage]>?
+//
+//    func loadFeed() {
+//        onLoadingStateChange?(true)
+//        feedLoader.load{[weak self] result in
+//            guard let self else {
+//                return
+//            }
+//            if let feed = try? result.get() {
+//                onFeedLoad?(feed)
+//            }
+//            onLoadingStateChange?(false)
+//        }
+//    }
+//}
